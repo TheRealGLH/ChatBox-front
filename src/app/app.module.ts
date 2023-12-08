e7f9ba357d30784ae44719f6fc2e0c22f4dc809e
TheRealGLH <10632069+TheRealGLH@users.noreply.github.com>
2023-12-08 20:43:36 +0100

    Cleans up sign in ui a bit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -32,7 +32,7 @@ const firebaseUiAuthConfig: firebaseui.auth.Config = {
   signInFlow: 'popup',
   signInOptions: [
       firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      {
+      /*{
           scopes: [
               'public_profile',
               'email',
@@ -44,21 +44,18 @@ const firebaseUiAuthConfig: firebaseui.auth.Config = {
           },
           provider:       firebase.auth.FacebookAuthProvider.PROVIDER_ID
       },
-      firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-      firebase.auth.GithubAuthProvider.PROVIDER_ID,
+      firebase.auth.GithubAuthProvider.PROVIDER_ID,*/
       {
           requireDisplayName: false,
           provider: firebase.auth.EmailAuthProvider.PROVIDER_ID
-      },
-      firebase.auth.PhoneAuthProvider.PROVIDER_ID,
-      firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
+      }
   ],
   //term of service
   tosUrl: '<your-tos-link>',
   //privacy url
   privacyPolicyUrl: '<your-privacyPolicyUrl-link>',
   //credentialHelper:             firebaseui.auth.CredentialHelper.ACCOUNT_CHOOSER_COM
-  credentialHelper: firebaseui.auth.CredentialHelper.NONE
+  credentialHelper: firebaseui.auth.CredentialHelper.GOOGLE_YOLO
 };
 
 @NgModule({
