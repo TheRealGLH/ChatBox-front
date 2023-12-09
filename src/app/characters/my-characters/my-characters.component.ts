23131127b90329b915cb3ac1821bd4ea63b95597
TheRealGLH <10632069+TheRealGLH@users.noreply.github.com>
2023-12-09 11:45:22 +0100

    character page components routed

---
+++ src/app/characters/my-characters/my-characters.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-characters',
+  standalone: true,
+  imports: [],
+  templateUrl: './my-characters.component.html',
+  styleUrl: './my-characters.component.css'
+})
+export class MyCharactersComponent {
+
+}
