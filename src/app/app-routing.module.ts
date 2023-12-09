29179fabeff9f2985af19d8c91455ef925be28f1
TheRealGLH <10632069+TheRealGLH@users.noreply.github.com>
2023-12-09 15:15:24 +0100

    Moves new char component to a dialog

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { NotFoundComponent } from './not-found/not-found.component';
-import { NewCharacterComponent } from './characters/new-character/new-character.component';
 import { ViewCharacterComponent } from './characters/view-character/view-character.component';
 import { EditCharacterComponent } from './characters/edit-character/edit-character.component';
 import { MyCharactersComponent } from './characters/my-characters/my-characters.component';
@@ -11,7 +10,6 @@ import { MyCharactersComponent } from './characters/my-characters/my-characters.
 const routes: Routes = [
   { path: '', component: HomePageComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'character/new', component: NewCharacterComponent},
   { path: 'character/mine', component: MyCharactersComponent},
   { path: 'character/view/:characterId', component: ViewCharacterComponent},
   { path: 'character/edit/:characterId', component: EditCharacterComponent},
