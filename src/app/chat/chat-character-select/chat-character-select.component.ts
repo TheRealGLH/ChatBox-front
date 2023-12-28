c9f7914476912fb14636f1182e7884da97b6b043
TheRealGLH <10632069+TheRealGLH@users.noreply.github.com>
2023-12-28 13:12:38 +0100

    Character select gui for chat

--- src/app/chat/chat-character-select/chat-character-select.component.ts
+++ src/app/chat/chat-character-select/chat-character-select.component.ts
@@ -1,11 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 import { Character } from 'src/models/character/character';
 
 @Component({
   selector: 'app-chat-character-select',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule, MatFormFieldModule, MatSelectModule, MatInputModule],
   templateUrl: './chat-character-select.component.html',
   styleUrl: './chat-character-select.component.css'
 })
