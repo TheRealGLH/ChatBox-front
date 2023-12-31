5bd7b4acf5a8c01317bd2e8ed2c3598a1633744f
TheRealGLH <10632069+TheRealGLH@users.noreply.github.com>
2023-12-31 16:04:10 +0100

    Sending messages from chat view

--- src/app/chat/chat-main/chat-main.component.ts
+++ src/app/chat/chat-main/chat-main.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
@@ -8,12 +9,18 @@ import { ChatMessage } from 'src/models/chat/chat-message';
 @Component({
   selector: 'app-chat-main',
   standalone: true,
-  imports: [CommonModule, MatIconModule, MatButtonModule, MatInputModule],
+  imports: [CommonModule, MatIconModule, MatButtonModule, MatInputModule, ReactiveFormsModule],
   templateUrl: './chat-main.component.html',
   styleUrl: './chat-main.component.css'
 })
 export class ChatMainComponent {
   @Input() chatMessages: ChatMessage[] = [];
+  @Output() messageEvent = new EventEmitter<string>();
+  formControlChat = new FormControl('', [
+    Validators.required,
+    Validators.minLength(2),
+    Validators.maxLength(1024),
+  ]);
 
   constructor(){
     /*
@@ -21,6 +28,11 @@ export class ChatMainComponent {
       let msg: ChatMessage = new ChatMessage(Date.now(),"Hello, I'm a test message or something", "314154r3eq","Fuckhead");
       this.chatMessages.push(msg);
     }*/
+  }
+
+  _sendMessage(message: string){
+    this.messageEvent.emit(message);
+    this.formControlChat.setValue('');
   }
 
 }
